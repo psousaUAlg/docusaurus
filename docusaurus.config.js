4a0e4e71997a2fe3ff1b45c1f1c8db52e07d8bdc
psousaUAlg <a74558@ualg.pt>
2023-12-20 13:57:50 +0000

    Docusaurus.config.js preparado para deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,17 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://psousaUAlg.github.io',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docusaurus/',
-  deploymentBranch: 'gh-pages',
-
-
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'psousaUAlg', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
